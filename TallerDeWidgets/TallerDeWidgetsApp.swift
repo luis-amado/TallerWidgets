fce7c6bdef2f978f1417c535f9d13d2902f0656a
Luis <lamadoalvarez@gmail.com>
2024-12-04 17:35:35 -0600

    Initial Commit

---
+++ TallerDeWidgets/TallerDeWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TallerDeWidgetsApp.swift
+//  TallerDeWidgets
+//
+//  Created by Luis Amado on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TallerDeWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
